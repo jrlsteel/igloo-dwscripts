ca1bede441a597db977fad7e68f09e212f1468f9
sakthimurugan <sakthi.murugan@igloo.energy>
2018-10-09 12:47:55 +0100

    Changed the Meters audit sql for performance

--- Audit/Ensek_metersAudit.sql
+++ Audit/Ensek_metersAudit.sql
@@ -51,36 +51,51 @@ alter table ref_meters_audit owner to igloo
 ;
 
 
-
 -- Updated Meters SQL
--- insert into ref_meters_audit
-select
-	s.account_id,
-	s.meter_point_id,
-	s.meterid,
-	s.meterserialnumber,
-	cast (nullif(s.installeddate,'') as timestamp) as installeddate,
-	cast (nullif(s.removeddate,'') as timestamp) as removeddate,
-	case when r.meter_id is null then 'n' else 'u' end as etlchangetype,
-	current_timestamp as etlchange
-from aws_s3_ensec_api_extracts.cdb_meters s
-left outer join ref_meters r
-      ON s.account_id = r.account_id
-      and s.meter_point_id = r.meter_point_id
-      and s.meterid = r.meter_id
-where (s.meterserialnumber != r.meterserialnumber
-			or cast (nullif(s.installeddate, '') as timestamp) != r.installeddate
-			or cast (nullif(s.removeddate, '') as timestamp) != r.removeddate
-			or r.meter_id is null);
+-- insert into ref_meters_audit (
+select cast(s.account_id as bigint) as account_id,
+       cast(s.meter_point_id as bigint) as meter_point_id,
+       cast(s.meterid as bigint) as meterid,
+       trim(s.meterserialnumber) as meterserialnumber,
+       cast(nullif(s.installeddate, '') as timestamp) as installeddate,
+       cast(nullif(s.removeddate, '') as timestamp) as removeddate,
+       case when r.meter_id is null then 'n' else 'u' end as etlchangetype,
+       current_timestamp as etlchange
+from aws_s3_ensec_api_extracts.cdb_stagemeters s
+       left outer join ref_meters r ON
+        cast(s.account_id as bigint) = r.account_id
+        and cast(s.meter_point_id as bigint) = r.meter_point_id
+        and cast(s.meterid as bigint) = r.meter_id
+where trim(s.meterserialnumber) != trim(r.meterserialnumber)
+   or cast(nullif(s.installeddate, '') as timestamp) != r.installeddate
+   or cast(nullif(s.removeddate, '') as timestamp) != r.removeddate
+   or r.meter_id is null
+--      )
+;
 
+-- overwrite for ref table
 -- delete from ref_meters;
--- insert into
-select
-	s.account_id as account_id,
-	s.meter_point_id as meter_point_id,
-	s.meterid as meter_id,
-	s.meterserialnumber as meterserialnumber,
-	cast (nullif(s.installeddate,'') as timestamp) installeddate,
-	cast (nullif(s.removeddate,'') as timestamp) removeddate
-from aws_s3_ensec_api_extracts.cdb_meters s;
+-- insert into ref_meters
+select cast(s.account_id as bigint) as account_id,
+       cast(s.meter_point_id as bigint) as meter_point_id,
+       cast(s.meterid as bigint) as meterid,
+       trim(s.meterserialnumber) as meterserialnumber,
+       cast(nullif(s.installeddate, '') as timestamp) as installeddate,
+       cast(nullif(s.removeddate, '') as timestamp) as removeddate
+from aws_s3_ensec_api_extracts.cdb_stagemeters s;
 
+
+select count(*) from aws_s3_ensec_api_extracts.cdb_stagemeters; --36578
+select count(*) from ref_meters r; -- 0
+
+select r.etlchangetype, r.etlchange, count(*) from ref_meters_audit r group by r.etlchangetype, r.etlchange; -- 36702
+
+
+--TESTING--
+-- test new rows
+delete from ref_meters where account_id=11869; -- 3 rows
+-- test updated rows
+update ref_meters set removeddate = current_timestamp where account_id = 6602;-- 3 rows
+
+select * from aws_s3_ensec_api_extracts.cdb_stagemeters s where s.account_id = 6602;
+select * from ref_meters s where s.account_id = 6602;
