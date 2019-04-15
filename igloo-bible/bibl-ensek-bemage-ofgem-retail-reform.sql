3717325a47c16739275ab9d2877f8422613631b9
Jonathan Steel <jonathan.steel@igloo.energy>
2019-04-15 10:06:24 +0100

    DMRE-392

--- igloo-bible/bibl-ensek-bemage-ofgem-retail-reform.sql
+++ igloo-bible/bibl-ensek-bemage-ofgem-retail-reform.sql
@@ -335,3 +335,90 @@ group by case
 
 
 
+select 'Igloo Energy'                  as supplier_name,
+       getdate()                       as date,
+       '1-Gas'                         as gas_tariff_uid_1,
+       '1-Elec'                        as electricity_tariff_uid_1,
+       null                             as electricity_tariff_uid_2,
+       case
+         when rcam.meterpoint_attribute_elec_gsp = '_A' then 'east_england'
+         when rcam.meterpoint_attribute_elec_gsp = '_B' then 'east_midlands'
+         when rcam.meterpoint_attribute_elec_gsp = '_C' then 'london'
+         when rcam.meterpoint_attribute_elec_gsp = '_D' then 'merseyside_and_north_wales'
+         when rcam.meterpoint_attribute_elec_gsp = '_E' then 'midlands'
+         when rcam.meterpoint_attribute_elec_gsp = '_F' then 'north_east'
+         when rcam.meterpoint_attribute_elec_gsp = '_G' then 'north_west'
+         when rcam.meterpoint_attribute_elec_gsp = '_H' then 'southern'
+         when rcam.meterpoint_attribute_elec_gsp = '_J' then 'south_east'
+         when rcam.meterpoint_attribute_elec_gsp = '_K' then 'south_wales'
+         when rcam.meterpoint_attribute_elec_gsp = '_L' then 'south_west'
+         when rcam.meterpoint_attribute_elec_gsp = '_M' then 'yorkshire'
+         when rcam.meterpoint_attribute_elec_gsp = '_N' then 'south_scotland'
+         when rcam.meterpoint_attribute_elec_gsp = '_P' then 'north_scotland'
+         else 'No Region Assigned'
+           end                                          as region,
+         'D' as payment_method_gas_1,
+         'D' as payment_method_electricity_1,
+         null as payment_method_electricity_2,
+         'N' as default_3_years_gas_1,
+         'N' as default_3_years_electricity_1,
+         null as default_3_years_electricity_2,
+        count(distinct(rcam.account_id))                 as number_of_accounts
+from ref_calculated_account_meterpoints rcam
+ inner join ref_tariff_history_gas_ur rthg on rcam.account_id = rthg.account_id and rthg.end_date is null
+where rcam.dual = 'dual_fuel'
+  and (rcam.supply_enddate_elec >= '2019-04-01' or rcam.supply_enddate_elec is null)
+  and (rcam.supply_enddate_gas >= '2019-04-01' or rcam.supply_enddate_gas is null)
+group by case
+           when rcam.meterpoint_attribute_elec_gsp = '_A' then 'east_england'
+           when rcam.meterpoint_attribute_elec_gsp = '_B' then 'east_midlands'
+           when rcam.meterpoint_attribute_elec_gsp = '_C' then 'london'
+           when rcam.meterpoint_attribute_elec_gsp = '_D' then 'merseyside_and_north_wales'
+           when rcam.meterpoint_attribute_elec_gsp = '_E' then 'midlands'
+           when rcam.meterpoint_attribute_elec_gsp = '_F' then 'north_east'
+           when rcam.meterpoint_attribute_elec_gsp = '_G' then 'north_west'
+           when rcam.meterpoint_attribute_elec_gsp = '_H' then 'southern'
+           when rcam.meterpoint_attribute_elec_gsp = '_J' then 'south_east'
+           when rcam.meterpoint_attribute_elec_gsp = '_K' then 'south_wales'
+           when rcam.meterpoint_attribute_elec_gsp = '_L' then 'south_west'
+           when rcam.meterpoint_attribute_elec_gsp = '_M' then 'yorkshire'
+           when rcam.meterpoint_attribute_elec_gsp = '_N' then 'south_scotland'
+           when rcam.meterpoint_attribute_elec_gsp = '_P' then 'north_scotland'
+           else 'No Region Assigned'
+    end
+;
+
+
+select 'Igloo Energy'                  as supplier_name,
+       getdate()                       as date,
+       '1-Gas'                         as gas_tariff_uid_1,
+       '1-Elec'                        as electricity_tariff_uid_1,
+       null                             as electricity_tariff_uid_2,
+       case
+         when mpa.attributes_attributevalue = '_A' then 'east_england'
+         when mpa.attributes_attributevalue = '_B' then 'east_midlands'
+         when mpa.attributes_attributevalue = '_C' then 'london'
+         when mpa.attributes_attributevalue = '_D' then 'merseyside_and_north_wales'
+         when mpa.attributes_attributevalue = '_E' then 'midlands'
+         when mpa.attributes_attributevalue = '_F' then 'north_east'
+         when mpa.attributes_attributevalue = '_G' then 'north_west'
+         when mpa.attributes_attributevalue = '_H' then 'southern'
+         when mpa.attributes_attributevalue = '_J' then 'south_east'
+         when mpa.attributes_attributevalue = '_K' then 'south_wales'
+         when mpa.attributes_attributevalue = '_L' then 'south_west'
+         when mpa.attributes_attributevalue = '_M' then 'yorkshire'
+         when mpa.attributes_attributevalue = '_N' then 'south_scotland'
+         when mpa.attributes_attributevalue = '_P' then 'north_scotland'
+           end                         as region,
+       count(distinct(rth.account_id)) as number_customer_accounts
+from ref_meterpoints mp
+       inner join ref_meterpoints_attributes mpa on mp.account_id = mpa.account_id
+       inner join ref_tariff_history rth on mpa.account_id = rth.account_id
+       inner join ref_meterpoints mp1 on mp.account_id = mp1.account_id
+       inner join vw_acl_reg_gaselec_happy vreh on mp.account_id = vreh.account_id
+WHERE mp.meterpointtype = 'E'
+  and (mp.supplyenddate is null or mp.supplyenddate >= '2019-04-01')
+  and mp1.meterpointtype = 'G'
+  and (mp1.supplyenddate is null or mp1.supplyenddate > '2019-04-01')
+  and mpa.attributes_attributename = 'GSP'
+group by mpa.attributes_attributevalue
