51d49d934df1302f40616cbc321914353bd99fb7
Jonathan Steel <jonathan.steel@igloo.energy>
2019-05-31 15:21:41 +0100

    DMRE-498

---
+++ nosi-flow/nosi_flow_tests.sql
@@ -0,0 +1,12 @@
+select count(*) from aws_s3_stage1_extracts.stage1_readingsnosigas;
+select count(*) from aws_s3_stage2_extracts.stage2_readingsnosigas;
+select count(*) from ref_readings_internal_nosi;
+select count(*) from ref_readings_internal;
+select count(*) from vw_ref_readings_all;
+select count(*) from ref_readings_internal_valid;
+select count(*) from vw_ref_readings_all_valid;
+select count(*) from ref_calculated_aq;
+select count(*) from vw_ref_calculated_aq_all;
+select count(*) from ref_calculated_aq_v1;
+select count(*) from vw_ref_calculated_aq_v1_all;
+
