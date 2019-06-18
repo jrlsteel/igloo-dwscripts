45bdfca25b129805593be4319cb72fda8ee1a9e9
Jonathan Steel <jonathan.steel@igloo.energy>
2019-06-18 15:20:26 +0100

    DMRE-554

--- Consumption-Accuracy/ref_consumption_accuracy_elec.sql
+++ Consumption-Accuracy/ref_consumption_accuracy_elec.sql
@@ -143,8 +143,19 @@
 
 -- select * from ref_consumption_accuracy_elec
 -- where account_id =1831
-
-select x.account_id                                                                                                                               as account_id,
+create view vw_ref_consumption_accuracy_elec as
+select con_elec.account_id,
+       con_elec.reading_datetime,
+       rest.quote_firsteac_var,
+       rest.ind_estimate_frequency as estimate_frequency,
+       rest.igloo_estimate_age as estimate_age,
+       con_elec.pa_cons_elec,
+       con_elec.igl_ind_eac,
+       con_elec.ind_eac,
+       con_elec.quotes_eac,
+       con_elec.etlchange
+from
+     (select x.account_id                                                                                                                               as account_id,
        max(x.supplystartdate)                                                                                                                     as supplystartdate,
        max(x.supplyenddate)                                                                                                                       as supplyenddate,
        max(x.associationstartdate)                                                                                                                as assocstartdate,
@@ -158,7 +169,7 @@ select x.account_id                                                             
         from ref_estimates_elec_internal rei
         where rei.account_id = x.account_id
           and rei.effective_from >= max(x.supplystartdate))                                                                                       as ind_estimate_count,
-       max(ind_firsteac_acc)                                                                                                                      as ind_firsteac,
+       max(ind_firsteac_acc)                                                                                                                      as ind_firsteac_read_based_acc,
        round(case
                when max(ind_firsteac_acc) > 0
                        then 100 -
@@ -174,10 +185,7 @@ select x.account_id                                                             
                                                                                           from ref_estimates_elec_internal rei
                                                                                           where rei.account_id = x.account_id
                                                                                             and rei.effective_from >= max(x.supplystartdate)) end as ind_estimate_frequency,
-       datediff(day, getdate(), x.latest_reading_datetime)                                                                                        as igloo_estimate_age,
-       max(x.pa_cons_elec_acc)                                                                                                                    as pa_cons_elec,
-       max(x.igl_ind_eac_acc)                                                                                                                     as igl_ind_eac,
-       max(x.ind_eac_acc)                                                                                                                         as ind_eac,
+       datediff(day, x.latest_reading_datetime, getdate())                                                                                        as igloo_estimate_age,
        cast(max(x.quotes_eac_acc) as double precision)                                                                                            as quotes_eac,
        getdate()                                                                                                                                  as etlchange
 from (select reads.external_id                                                                             as account_id,
@@ -190,14 +198,8 @@ from (select reads.external_id                                                  
              reads.meterreadingdatetime                                                                    as reading_datetime,
              reads.latest_reading_datetime,
              reads.latest_read_per_register,
-             pa_eac.igloo_eac                                                                              as pa_cons_elec_reg,
-             ig_eac.igloo_eac_v1                                                                           as igl_ind_eac_reg,
              ee.estimation_value                                                                           as ind_eac_reg,
              q.electricity_usage                                                                              quotes_eac,
-             sum(
-               pa_eac.igloo_eac) over (partition by reads.external_id, reads.latest_read_per_register)     as pa_cons_elec_acc,
-             sum(
-               ig_eac.igloo_eac_v1) over (partition by reads.external_id, reads.latest_read_per_register)  as igl_ind_eac_acc,
              sum(
                ee.estimation_value) over (partition by reads.external_id, reads.latest_read_per_register)  as ind_eac_acc,
              max(
@@ -229,13 +231,7 @@ from (select reads.external_id                                                  
                    inner join ref_registers reg on reg.account_id = su.external_id and reg.meter_id = mt.meter_id
                    inner join ref_readings_internal_valid ri
                      on ri.account_id = su.external_id and ri.register_id = reg.register_id
-            where mp.account_id = 1831) reads
-             left outer join ref_calculated_eac_audit pa_eac
-               on pa_eac.account_id = reads.external_id and reads.register_id = pa_eac.register_id and
-                  reads.meterreadingdatetime = pa_eac.read_max_created_date_elec
-             left outer join ref_calculated_eac_v1_audit ig_eac
-               on ig_eac.account_id = reads.external_id and reads.register_id = ig_eac.register_id and
-                  reads.meterreadingdatetime = ig_eac.read_max_datetime_elec
+            where mp.account_id =2117) reads
              left outer join ref_estimates_elec_internal ee
                on ee.account_id = reads.external_id and ee.mpan = reads.meterpointnumber and
                   ee.register_id = reads.registers_registerreference and
@@ -243,21 +239,68 @@ from (select reads.external_id                                                  
                   ee.effective_from = reads.meterreadingdatetime
              left outer join ref_estimates_elec_internal ee1
                on ee1.account_id = reads.external_id and ee1.mpan = reads.meterpointnumber and
-                  ee1.register_id = reads.registers_registerreference and
-                  ee1.serial_number = reads.meterserialnumber and
-                  ee1.effective_from = reads.earliest_reading_datetime
+                  --   ee1.register_id = reads.registers_registerreference and
+                  --    ee1.serial_number = reads.meterserialnumber and
+                  ee1.effective_from = greatest(reads.associationstartdate, reads.supplystartdate)
              left outer join ref_cdb_registrations creg on creg.id = reads.registration_id
              left outer join ref_cdb_quotes q on q.id = creg.quote_id) x
 where x.latest_read_per_register = 1
     -- and x.account_id in (1831, 23983, 1893,1854)
 group by x.account_id, x.latest_reading_datetime
-order by x.account_id, x.latest_reading_datetime
+order by x.account_id, x.latest_reading_datetime) rest
+left outer join ref_consumption_accuracy_elec con_elec on rest.account_id = con_elec.account_id
+
+--
+--
+--
+--
+-- SELECT account_id, reading_datetime, pa_cons_elec, igl_ind_eac, ind_eac, quotes_eac, etlchange
+-- FROM ref_consumption_accuracy_elec con_elec
+-- left outer join  ref_meterpoints mp on con_elec.account_id = mp.account_id;
+--
+--
+--
+--
+--
+-- select * from ref_consumption_accuracy_elec
+-- where account_id=2117
 
 
-SELECT account_id, reading_datetime, pa_cons_elec, igl_ind_eac, ind_eac, quotes_eac, etlchange
-FROM ref_consumption_accuracy_elec
-where account_id = 1831;
 
+
+
+
+
+
+select * from ref_estimates_elec_internal
+where account_id = 2117
+--and islive = true
+order by effective_from asc
+
+select * from ref_meterpoints
+where account_id =2117
+
+select * from ref_meters
+where account_id = 2117
+
+select * from ref_estimates_elec_internal
+where account_id = 1831
+--and islive = true
+order by effective_from asc
+
+
+
+
+select * from ref_meterpoints
+where account_id =1831
+
+select account_id
+from ref_meterpoints mp
+       inner join (select account_id, count(*)
+                   from ref_registers
+                   group by account_id
+                   having count(*) > 3
+                   order by account_id desc) reg on mp.account_id = reg.account_id
 
 
 
