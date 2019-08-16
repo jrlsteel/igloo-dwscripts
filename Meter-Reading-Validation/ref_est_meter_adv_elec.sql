1b5a885d99e130cbf6b214f91a2cdfa6bdff050e
BenJesty <ben.jesty@igloo.energy>
2019-08-16 11:23:21 +0100

    Modified coefficient selection method to "fill in" missing ppc values with 1/365, primarily to cover the missing values from yesterday

--- Meter-Reading-Validation/ref_est_meter_adv_elec.sql
+++ Meter-Reading-Validation/ref_est_meter_adv_elec.sql
@@ -1,4 +1,4 @@
-insert into ref_estimated_advance_elec
+--insert into ref_estimated_advance_elec
 select x1.account_id,
        x1.register_id,
        x1.last_reading_date,
@@ -19,20 +19,25 @@ from (select account_id,
              last_reading_date,
              last_reading_value,
              days_since_last_read,
-             round(coalesce(eac, 0), 0)                                         as eac,
-             round(coalesce(igl_ind_eac, 0), 0)                                 as igl_ind_eac,
-             round(coalesce(ind_eac, 0), 0)                                     as ind_eac,
-             getdate()                                                          as effective_for,
-             (case
-                  when ppc is null or ppc_count < days_since_last_read
-                      then eac * bpp
-                  else eac * ppc end)                                           as igl_estimated_advance,
-             (ind_eac * (cast(days_since_last_read as double precision) / 365)) as ind_estimated_advance
+             round(coalesce(eac, 0), 0)                                                   as eac,
+             round(coalesce(igl_ind_eac, 0), 0)                                           as igl_ind_eac,
+             round(coalesce(ind_eac, 0), 0)                                               as ind_eac,
+             getdate()                                                                    as effective_for,
+          /*(case
+               when ppc is null or ppc_count < days_since_last_read
+                   then eac * bpp
+               else eac * ppc end)           as igl_estimated_advance,
+          (case
+               when ppc is null or ppc_count < days_since_last_read
+                   then ind_eac * bpp
+               else ind_eac * ppc end)       as ind_estimated_advance,*/
+             eac * (nvl(ppc, 0) + ((days_since_last_read - nvl(ppc_count, 0)) / 365.0))     as igl_estimated_advance,
+             ind_eac * (nvl(ppc, 0) + ((days_since_last_read - nvl(ppc_count, 0)) / 365.0)) as ind_estimated_advance
       from (select su.external_id                                                        as account_id,
                    r.register_id,
-                   reads.meterreadingdatetime                                            as last_reading_date,
-                   reads.readingvalue                                                    as last_reading_value,
-                   datediff(days, reads.meterreadingdatetime, trunc(getdate()))          as days_since_last_read,
+                   latest_read.meterreadingdatetime                                      as last_reading_date,
+                   latest_read.readingvalue                                              as last_reading_value,
+                   datediff(days, latest_read.meterreadingdatetime, trunc(getdate()))    as days_since_last_read,
                    coalesce(eac.igl_ind_eac, 0)                                          as igl_ind_eac,
                    coalesce(r.registers_eacaq, 0)                                        as ind_eac,
                    coalesce(nullif(eac.igl_ind_eac, 0), nullif(r.registers_eacaq, 0), 0) as eac,
@@ -42,8 +47,8 @@ from (select account_id,
                       and ss_conf_id = rma_ssc.attributes_attributevalue
                       and cast(time_pattern_regime as bigint) = r.registers_tpr
                       and pcl_id = cast(rma_pcl.attributes_attributevalue as integer)
-                      and st_date >= trunc(min(reads.meterreadingdatetime))
-                      and st_date < trunc(max(reads.meterreadingdatetime))
+                      and st_date >= trunc(latest_read.meterreadingdatetime)
+                      and st_date < trunc(current_date)
                     group by gsp_group_id, ss_conf_id)                                   as ppc,
                    (select count(*)
                     from ref_d18_igloo_ppc
@@ -51,16 +56,16 @@ from (select account_id,
                       and ss_conf_id = rma_ssc.attributes_attributevalue
                       and cast(time_pattern_regime as bigint) = r.registers_tpr
                       and pcl_id = cast(rma_pcl.attributes_attributevalue as integer)
-                      and st_date >= trunc(min(reads.meterreadingdatetime))
-                      and st_date < trunc(max(reads.meterreadingdatetime))
+                      and st_date >= trunc(latest_read.meterreadingdatetime)
+                      and st_date < trunc(current_date)
                     group by gsp_group_id, ss_conf_id)                                   as ppc_count,
                    (select sum(bpp_sum)
                     from ref_d18_igloo_bpp
                     where gsp_group_id = rma_gsp.attributes_attributevalue
                       and pcl_id = cast(rma_pcl.attributes_attributevalue as integer)
                       and pfl_id = 1
-                      and st_date >= trunc(min(reads.meterreadingdatetime))
-                      and st_date < trunc(max(reads.meterreadingdatetime))
+                      and st_date >= trunc(latest_read.meterreadingdatetime)
+                      and st_date < trunc(current_date)
                     group by gsp_group_id)                                               as bpp
             from ref_cdb_supply_contracts su
                      inner join ref_meterpoints mp on mp.account_id = su.external_id and mp.meterpointtype = 'E' and
@@ -91,20 +96,20 @@ from (select account_id,
                                               over (partition by ri.account_id, register_id order by meterreadingdatetime desc) rownum
                                        from ref_readings_internal_valid ri
                                        order by ri.meterreadingdatetime desc) r1
-                                 where r1.rownum = 1) reads
-                                on reads.account_id = su.external_id and reads.register_id = r.register_id
+                                 where r1.rownum = 1) latest_read
+                                on latest_read.account_id = su.external_id and latest_read.register_id = r.register_id
                      left outer join ref_calculated_igl_ind_eac eac
                                      on eac.account_id = su.external_id and eac.register_id = r.register_id
-            group by su.external_id,
-                     r.register_id,
-                     reads.meterreadingdatetime,
-                     reads.readingvalue,
-                     eac.igl_ind_eac,
-                     r.registers_eacaq,
-                     r.registers_tpr,
-                     rma_gsp.attributes_attributevalue,
-                     rma_ssc.attributes_attributevalue,
-                     rma_pcl.attributes_attributevalue) x
+               /*group by su.external_id,
+                        r.register_id,
+                        latest_read.meterreadingdatetime,
+                        latest_read.readingvalue,
+                        eac.igl_ind_eac,
+                        r.registers_eacaq,
+                        r.registers_tpr,
+                        rma_gsp.attributes_attributevalue,
+                        rma_ssc.attributes_attributevalue,
+                        rma_pcl.attributes_attributevalue*/) x
       order by x.account_id, register_id) x1
          left join ref_stg_tolerances tol_elec
                    on tol_elec.tol_group = 'industry_tolerance_elec'
@@ -116,3 +121,4 @@ from (select account_id,
                    on rra.account_id = x1.account_id and rra.register_id = x1.register_id and
                       registersattributes_attributename = 'No_Of_Digits'
 order by x1.account_id, x1.register_id
+;
