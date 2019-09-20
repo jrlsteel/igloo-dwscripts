34cea2b61869691bd322ad7ce64aba8d11984f58
BenJesty <ben.jesty@igloo.energy>
2019-09-20 10:25:53 +0100

    Refined test cases

--- Release-Scripts/Release_V0-1-9.sql
+++ Release-Scripts/Release_V0-1-9.sql
@@ -1,16 +1,73 @@
+-- remove backups
+drop table ref_consumption_accuracy_elec_backup_R019;
+drop table ref_consumption_accuracy_gas_backup_R019;
+drop table ref_consumption_accuracy_elec_audit_backup_R019;
+drop table ref_consumption_accuracy_gas_audit_backup_R019;
+drop table ref_calculated_igl_ind_aq_backup_R019;
+drop table ref_calculated_aq_backup_R019;
+drop table ref_readings_internal_valid_backup_R019;
+
+
 -- DWH TABLE BACKUPS
-create table ref_consumption_accuracy_elec_backup_R019 as select * from ref_consumption_accuracy_elec;
-create table ref_consumption_accuracy_gas_backup_R019 as select * from ref_consumption_accuracy_gas;
-create table ref_consumption_accuracy_elec_audit_backup_R019 as select * from ref_consumption_accuracy_elec_audit;
-create table ref_consumption_accuracy_gas_audit_backup_R019 as select * from ref_consumption_accuracy_gas_audit;
-create table ref_calculated_igl_ind_aq_backup_R019 as select * from ref_calculated_igl_ind_aq;
-create table ref_calculated_aq_backup_R019 as select * from ref_calculated_aq;
-create table ref_readings_internal_valid_backup_R019 as select * from ref_readings_internal_valid;
+create table ref_consumption_accuracy_elec_backup_R019 as
+select *
+from ref_consumption_accuracy_elec;
+create table ref_consumption_accuracy_gas_backup_R019 as
+select *
+from ref_consumption_accuracy_gas;
+create table ref_consumption_accuracy_elec_audit_backup_R019 as
+select *
+from ref_consumption_accuracy_elec_audit;
+create table ref_consumption_accuracy_gas_audit_backup_R019 as
+select *
+from ref_consumption_accuracy_gas_audit;
+create table ref_calculated_igl_ind_aq_backup_R019 as
+select *
+from ref_calculated_igl_ind_aq;
+create table ref_calculated_aq_backup_R019 as
+select *
+from ref_calculated_aq;
+create table ref_readings_internal_valid_backup_R019 as
+select *
+from ref_readings_internal_valid;
+
 
 -- TRUNCATE TABLES
 truncate table ref_calculated_aq;
 truncate table ref_calculated_igl_ind_aq;
 truncate table ref_readings_internal_valid;
+
+
+--select max(etlchange) from "igloosense-preprod".public.ref_consumption_accuracy_gas
+-- revert tables to backups if necessary
+-- truncate table ref_calculated_aq;
+-- truncate table ref_calculated_igl_ind_aq;
+-- truncate table ref_readings_internal_valid;
+-- insert into ref_calculated_aq
+-- select *
+-- from ref_calculated_aq_backup_R019;
+-- insert into ref_calculated_igl_ind_aq
+-- select *
+-- from ref_calculated_igl_ind_aq_backup_R019;
+-- insert into ref_readings_internal_valid
+-- select *
+-- from ref_readings_internal_valid_backup_R019;
+-- truncate table ref_consumption_accuracy_elec;
+-- truncate table ref_consumption_accuracy_gas;
+-- truncate table ref_consumption_accuracy_elec_audit;
+-- truncate table ref_consumption_accuracy_gas_audit;
+-- insert into ref_consumption_accuracy_elec
+-- select *
+-- from ref_consumption_accuracy_elec_backup_R019;
+-- insert into ref_consumption_accuracy_gas
+-- select *
+-- from ref_consumption_accuracy_gas_backup_R019;
+-- insert into ref_consumption_accuracy_elec_audit
+-- select *
+-- from ref_consumption_accuracy_elec_audit_backup_R019;
+-- insert into ref_consumption_accuracy_gas_audit
+-- select *
+-- from ref_consumption_accuracy_gas_audit_backup_R019;
 
 -- NEW VIEWS
 -- 1
