f7235d78948242ace575a79d415fb25b6d0ddd08
Jonathan Steel <jonathan.steel@igloo.energy>
2019-10-24 15:22:35 +0100

    DMRE-867

--- Batch Optimisation/table-create-scripts-aurora.sql
+++ Batch Optimisation/table-create-scripts-aurora.sql
@@ -8,3 +8,29 @@ create table ref_occupier_accounts
 )
 ;
 
+create table ref_calculated_tariff_accounts
+(
+	account_id bigint null,
+	tariff_id int null,
+	start_date timestamp null,
+	end_date timestamp null
+)
+;
+
+create table ref_tariffs
+(
+	id int null,
+	fuel_type varchar(1) null,
+	gsp_ldz varchar(2) null,
+	name varchar(20) null,
+	billing_start_date timestamp null,
+	signup_start_date timestamp null,
+	end_date timestamp null,
+	standing_charge double null,
+	unit_rate double null,
+	discounts varchar(50) null,
+	tariff_type varchar(20) null,
+	exit_fees double null
+)
+;
+
