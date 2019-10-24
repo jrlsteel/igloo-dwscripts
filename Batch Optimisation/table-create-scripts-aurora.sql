f17aa15854651153b2d7b8febd8d63bdf9df4604
Jonathan Steel <jonathan.steel@igloo.energy>
2019-10-24 11:20:53 +0100

    DMRE-860

---
+++ Batch Optimisation/table-create-scripts-aurora.sql
@@ -0,0 +1,10 @@
+create table ref_occupier_accounts
+(
+	account_id bigint null,
+	cot_date timestamp null,
+	current_balance double null,
+	days_since_cot bigint null,
+	etl_change timestamp null
+)
+;
+
