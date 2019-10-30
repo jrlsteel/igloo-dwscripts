f11da07258b5b41e690d7f57c9050782cafbd5d7
BenJesty <ben.jesty@igloo.energy>
2019-10-30 17:51:44 +0000

    DMRE-852 - added closed accounts to the hash list

--- reporting/synergy_email_hashes.sql
+++ reporting/synergy_email_hashes.sql
@@ -1,14 +1,16 @@
-
-select live_accounts.account_id,
-       users.email,
-       func_sha1(lower(users.email)) as hash
-from (select distinct account_id
-      from ref_meterpoints rm
-      where ((rm.supplyenddate is null and rm.associationenddate is null)
-          or least(supplyenddate, associationenddate) > getdate())) live_accounts
-         inner join ref_cdb_supply_contracts sc on live_accounts.account_id = sc.external_id
+select distinct rm.account_id,
+                users.email,
+                func_sha1(lower(users.email)) as hash,
+                case
+                    when max(nvl(least(rm.supplyenddate, rm.associationenddate), getdate() + 1)) > getdate()
+                        then 'current'
+                    else 'past' end           as customer_type
+from ref_meterpoints rm
+         inner join ref_cdb_supply_contracts sc on rm.account_id = sc.external_id
          inner join ref_cdb_user_permissions up on up.permissionable_type = 'App\\SupplyContract' and
                                                    up.permissionable_id = sc.id
          inner join ref_cdb_users users on users.id = up.user_id
 --where account_id in (1831, 54977)
+group by rm.account_id, users.email
 order by account_id
+
