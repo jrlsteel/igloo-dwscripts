d03e6e3d434d3a751ba411da943616b91b9a397d
BenJesty <ben.jesty@igloo.energy>
2020-02-26 16:24:18 +0000

    DMRE-1096 - moved script to using stage2 mirror of cdb table rather than temp table

--- reporting/Grafana/SENS_Weekly/SENS_withdrawal_of_consent.sql
+++ reporting/Grafana/SENS_Weekly/SENS_withdrawal_of_consent.sql
@@ -1,5 +1,5 @@
 select mpd.id                             as ext_participant_id,
-       mpd.opted_out                      as consentwithdrawaldate,
+       nvl(mpd.opted_out, dcf.acc_ed)     as consentwithdrawaldate,
        addr.sub_building_name_number,
        addr.building_name_number,
        addr.dependent_thoroughfare,
@@ -15,7 +15,7 @@ select mpd.id                             as ext_participant_id,
        'TO BE ADDED'                      as gdprtype,
        listagg(mp.meterpointnumber, ', ') as mpan,
        addr.uprn
-from temp_meets_project_data mpd
+from aws_s3_stage2_extracts.stage2_cdbmeetsprojectdata mpd
          left join ref_cdb_addresses addr on mpd.address_id = addr.id
          left join ref_cdb_users u on mpd.user_id = u.id
          left join ref_cdb_user_permissions up_sc on up_sc.user_id = u.id and up_sc.permission_level = 0 and
@@ -25,7 +25,8 @@ from temp_meets_project_data mpd
                                          nvl(mpd.opted_out, trunc(getdate())) between
                                              greatest(supplystartdate, associationstartdate) and
                                              nvl(least(supplyenddate, associationenddate), getdate() + 1)
-where opted_out is not null
+         left join ref_calculated_daily_customer_file dcf on sc.external_id = dcf.account_id
+where consentwithdrawaldate is not null
 group by mpd.id, mpd.opted_out, addr.sub_building_name_number, addr.building_name_number, addr.dependent_thoroughfare,
          addr.thoroughfare, addr.double_dependent_locality, addr.dependent_locality, addr.post_town, addr.county,
          addr.postcode, u.first_name, u.last_name, addr.uprn
