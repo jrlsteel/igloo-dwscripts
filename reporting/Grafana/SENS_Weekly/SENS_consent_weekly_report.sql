5394f55bf4b4b2444aab2cbdf890d53aa11ecf1a
BenJesty <ben.jesty@igloo.energy>
2020-02-26 16:26:49 +0000

    DMRE-1095 - moved to using stage2 mirror of cdb table rather than temp table

--- reporting/Grafana/SENS_Weekly/SENS_consent_weekly_report.sql
+++ reporting/Grafana/SENS_Weekly/SENS_consent_weekly_report.sql
@@ -1,4 +1,4 @@
-select mpd.id                                             as ext_participant_id,
+select mpd.id                                                    as ext_participant_id,
        addr.sub_building_name_number,
        addr.building_name_number,
        addr.dependent_thoroughfare,
@@ -10,23 +10,24 @@ select mpd.id                                             as ext_participant_id,
        addr.postcode,
        u.first_name,
        u.last_name,
-       listagg(mp.meterpointnumber, ', ')                 as mpan,
+       listagg(mp.meterpointnumber, ', ')                        as mpan,
        addr.uprn,
-       mpd.opted_out is null or mpd.opted_out > getdate() as consent_provided,
-       mpd.opted_in                                       as consent_start_date,
-       mpd.opted_out                                      as consent_end_date,
-       'Igloo'                                            as consent_source,
+       nvl(mpd.opted_out, dcf.acc_ed, getdate() + 1) > getdate() as consent_provided,
+       mpd.opted_in                                              as consent_start_date,
+       nvl(mpd.opted_out, dcf.acc_ed)                            as consent_end_date,
+       'Igloo'                                                   as consent_source,
        mpd.move_in_date,
-       'TO BE ADDED'                                      as trial_id,
-       mpd."group"                                        as trial_group
-from temp_meets_project_data mpd
+       'sens-igloo'                                              as trial_id,
+       mpd."group"                                               as trial_group
+from aws_s3_stage2_extracts.stage2_cdbmeetsprojectdata mpd
          left join ref_cdb_addresses addr on mpd.address_id = addr.id
          left join ref_cdb_users u on mpd.user_id = u.id
          left join ref_cdb_user_permissions up_sc on up_sc.user_id = u.id and up_sc.permission_level = 0 and
                                                      up_sc.permissionable_type ilike 'app%supplycontract'
          left join ref_cdb_supply_contracts sc on up_sc.permissionable_id = sc.id
+         left join ref_calculated_daily_customer_file dcf on sc.external_id = dcf.account_id
          left join ref_meterpoints mp on mp.account_id = sc.external_id and mp.meterpointtype = 'E' and
-                                         nvl(mpd.opted_out, trunc(getdate())) between
+                                         nvl(mpd.opted_out, dcf.acc_ed, trunc(getdate())) between
                                              greatest(supplystartdate, associationstartdate) and
                                              nvl(least(supplyenddate, associationenddate), getdate() + 1)
 group by mpd.id, addr.sub_building_name_number, addr.building_name_number, addr.dependent_thoroughfare,
