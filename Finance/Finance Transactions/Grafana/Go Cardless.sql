682936763b8e934d5cc8a564ec9ae9c956b066ba
tolulopeadegunle <tolulope.adegunle@igloo.energy>
2020-04-16 12:26:03 +0100

    DMRE-1163

--- Finance/Finance Transactions/Grafana/Go Cardless.sql
+++ Finance/Finance Transactions/Grafana/Go Cardless.sql
@@ -1,110 +1,219 @@
-; with cte_gc as (
---- GC Tab ---
-select
- gc.ensekAccountId ,
- gc.created_at,
- gc.payments_amount::float,
- SUM(CASE WHEN gc.ensekAccountId = ensek.AccountId and gc.ensekAccountId is not null
-          THEN 1
-          ELSE 0
-      END ) as Countif
-from
-       (
+; with cte_refunds_go as (
+       select
+         *
+        from public.vw_fin_trans_refunds ref
+       where ref.created_at between '$StartDate' and '$EndDate'
+
+      )
+
+,  cte_payments_go as (
+       select
+        *
+        from public.vw_fin_trans_payments py
+       where py.created_at between '$StartDate' and '$EndDate'
+
+      )
+
+, cte_ensek_go as (
+        select *
+        from public.vw_fin_trans_ensek
+      where (accountid is not null or createddate is not null or transamount is not null)
+        and createddate between dateadd(day, 2, '$StartDate') and  dateadd(day, -2, '$EndDate')
+      )
+
+,  cte_payments_summ_go as (
+       select
+        ensekAccountId,
+        created_at,
+        "payments.amount",
+        count(*) as Countif
+        from cte_payments_go
+       where (ensekAccountId is not null)
+       and created_at between dateadd(day, 2, '$StartDate') and  dateadd(day, -2, '$EndDate')
+       group by
+        ensekAccountId,
+        created_at ,
+        "payments.amount"
+      )
+
+,   cte_refunds_summ_go as (
+       select ref.ensekaccountid
+        , ref.created_at
+        , ref.refund_amount
+        , count(*) as Countif
+        from cte_refunds_go ref
+       WHERE (ref.ensekaccountid is not null)
+       and ref.created_at between dateadd(day, 2, '$StartDate') and  dateadd(day, -2, '$EndDate')
+       group by
+          ref.ensekaccountid
+        , ref.created_at
+        , ref.refund_amount
+      )
+
+
+, cte_ensek_summ_go as (
         select
-        "payments.metadata.accountid"  as ensekAccountId,
-        substring(created_at, 1, 10)::date as created_at,
-        "payments.description" as payments_amount,
-        "customers.given_name"  as "customers.id"
-        from aws_fin_stage1_extracts.fin_go_cardless_payments
-        )gc
-left join (
-            select
-                replace(accountid, ' ', '') as AccountId,
-                substring(replace(createddate, ' ', ''), 1, 10)::date as CreatedDate,
-                replace(transamount, ' ', '') as TransAmount
-                from aws_fin_stage1_extracts.fin_sales_ledger_journals
-             where accountid is not null or createddate is not null or transamount is not null
-             -- and lower(TransactionTypeName) = 'payment'
-             and lower(AccountDesc) = 'card provider cash'
-          ) ensek
-    on gc.ensekAccountId = ensek.AccountId and
-       gc.payments_amount = ensek.TransAmount and
-       gc.created_at between dateadd(day, -3, ensek.CreatedDate) and dateadd(day, 3, ensek.CreatedDate) --- -3 0r +3 days ---
-Group BY
-   gc.ensekAccountId ,
-   gc.created_at,
-   gc.payments_amount )
+        AccountId,
+        CreatedDate,
+        TransAmount,
+        count(*) as Countif
+        from cte_ensek_go
+       group by
+        AccountId,
+        CreatedDate,
+        TransAmount
+      )
 
 
+,   cte_ensek_tab_go as (
+      select
+       ensek.AccountId ,
+       ensek.CreatedDate,
+       ensek.TransAmount,
+       ref.refund_amount ,
+       paym."payments.amount" ,
+       paym.Countif as C_Pay,
+       ref.Countif as C_Ref
+      from cte_ensek_go ensek
+      left join cte_refunds_summ_go ref
+          on ref.ensekAccountId = ensek.AccountId and
+             ref.refund_amount = ensek.TransAmount
+      left join cte_payments_summ_go paym
+          on paym.ensekAccountId = ensek.AccountId and
+             paym."payments.amount" = ensek.TransAmount and
+             ensek.CreatedDate between dateadd(day, -3, paym.created_at) and dateadd(day, 3, paym.created_at) --- -3 0r +3 days ---
+      WHERE ensek.CreatedDate between dateadd(day, 2, '$StartDate') and  dateadd(day, -2, '$EndDate')
+ )
 
-, cte_ensek as (
+
+, cte_payments_tab_go as (
+    select
+     gc.ensekAccountId ,
+     gc.created_at,
+     gc."payments.amount",
+     --ensek.TransAmount,
+     ensek.Countif
+    from cte_payments_go gc
+    left join cte_ensek_summ_go ensek
+        on gc.ensekAccountId = ensek.AccountId and
+           gc."payments.amount" = ensek.TransAmount and
+           --gc.created_at = ensek.CreatedDate
+           gc.created_at between dateadd(day, -3, ensek.createdDate) and dateadd(day, 3, ensek.createdDate) --- -3 0r +3 days ---
+    WHERE gc.created_at between dateadd(day, 2, '$StartDate') and  dateadd(day, -2, '$EndDate')
+    )
+
+, cte_ensek_debit_credit_go as (
 --- Ensek Tab ---
 select
- ensek.AccountId ,
  ensek.CreatedDate,
- ensek.TransAmount::float,
- SUM(CASE WHEN gc.ensekAccountId = ensek.AccountId and gc.ensekAccountId is not null
-          THEN 1
-          ELSE 0
-      END ) as Countif
-from  (
-            select
-                replace(accountid, ' ', '') as AccountId,
-                substring(replace(createddate, ' ', ''), 1, 10)::date as CreatedDate,
-                replace(transamount, ' ', '') as TransAmount
-                from aws_fin_stage1_extracts.fin_sales_ledger_journals
-                where --lower(TransactionTypeName) = 'payment' and
-                lower(AccountDesc) = 'card provider cash'
-          ) ensek
-left join
-         (
-          select
-          "payments.metadata.accountid"  as ensekAccountId,
-          substring(created_at, 1, 10)::date as created_at,
-          "payments.description" as payments_amount,
-          "customers.given_name"  as "customers.id"
-          from aws_fin_stage1_extracts.fin_go_cardless_payments
-         where ("payments.metadata.accountid" is not null ) or
-               (created_at is not null  or  created_at != '')or
-               ("customers.given_name" is not null or "customers.given_name" != '')
-          )gc
-    on gc.ensekAccountId = ensek.AccountId and
-       gc.payments_amount = ensek.TransAmount and
-       ensek.CreatedDate between dateadd(day, -3, gc.created_at) and dateadd(day, 3, gc.created_at) --- -3 0r +3 days ---
-Group BY
-   ensek.AccountId ,
-   ensek.CreatedDate,
-   ensek.TransAmount )
-
-
-
-, c_rec as (
-    SELECT ROW_NUMBER() OVER (ORDER BY c.accountid)
-    from aws_fin_stage1_extracts.fin_sales_ledger_journals c
-    limit 1000
+  SUM(CASE WHEN TransAmount::float > 0 THEN TransAmount::float ELSE null END) as Debits,
+  SUM(CASE WHEN TransAmount::float < 0 THEN TransAmount::float ELSE null END) as Credits
+from  cte_ensek_go ensek
+Group  By ensek.CreatedDate
 )
 
-,  cte_date as (SELECT substring(DATEADD(DAY, nbr - 1, '2020-01-01'), 1, 10) as date_datetime
-                FROM (SELECT ROW_NUMBER() OVER (ORDER BY c.row_number) AS Nbr FROM c_rec c) nbrs
-                WHERE nbr - 1 <= DATEDIFF(DAY, '2019-12-01', '2021-03-01'))
+
+, cte_date_go as (
+       select distinct CreatedDate as date_datetime
+          from aws_fin_stage1_extracts.fin_sales_ledger_all_time
+          where  CreatedDate between '$StartDate' and '$EndDate'
+      )
+
+, cte_ensek_report_go as (
+select cd.date_datetime as ensek_date, ensek.Debits, ensek.Credits
+ from cte_date_go cd
+        left join cte_ensek_debit_credit_go ensek
+        on ensek.CreatedDate = cd.date_datetime
+      )
+
+, cte_debits_report_go as (
+      select
+      cd.date_datetime as gc_date,
+      case when pay.Payments > ensek.Debits then pay.Payments - ensek.Debits else null end as "In GC not in Ensek" ,
+      case when pay.Payments < ensek.Debits then ensek.Debits - pay.Payments else null end as "In Ensek not in GC"
+      from
+      cte_date_go cd
+      left join
+          (
+            select
+            gc.created_at,
+            sum(gc."payments.amount"::float) as Payments
+            from
+            cte_payments_go gc
+            where gc.created_at between dateadd(day, 2, '$StartDate') and  dateadd(day, -2, '$EndDate')
+            group by
+            gc.created_at
+          ) pay
+       on pay.created_at = cd.date_datetime
+      left join
+          cte_ensek_debit_credit_go ensek
+       on ensek.CreatedDate = cd.date_datetime
+   )
 
 
+, cte_goCardless_go as (
+       select
+        cd.date_datetime as gc_date,
+        pay.Payments,
+        ref.Refunds
+        from
+        cte_date_go cd
+        left join
+            (
+              select
+              gc.created_at,
+              sum(gc."payments.amount"::float) as Payments
+              from
+              cte_payments_go gc
+              where gc.created_at between dateadd(day, 2, '$StartDate') and  dateadd(day, -2, '$EndDate')
+              group by
+              gc.created_at
+            ) pay
+         on pay.created_at = cd.date_datetime
+        left join
+            (
+              select
+              gc.created_at,
+              sum(gc.refund_amount::float) as Refunds
+              from
+              cte_refunds_go gc
+              where gc.created_at between dateadd(day, 2, '$StartDate') and  dateadd(day, -2, '$EndDate')
+              group by
+              gc.created_at
+            ) ref
+         on ref.created_at = cd.date_datetime
+      )
+
+, cte_goCardless_total_go as (
+    select
+    gc_date,
+    Payments,
+    Refunds,
+    0 as rnk
+    from cte_goCardless_go
+
+    UNION
+
+    select
+    'Total:' as gc_date,
+    sum(Payments) as Payments,
+    sum(Refunds) as Refunds,
+    1 as rnk
+    from cte_goCardless_go
+
+    UNION
+
+    select
+    'Net Total:' as gc_date,
+    null           as Payments,
+    sum(Payments) + sum(Refunds) as Refunds,
+    2 as rnk
+    from cte_goCardless_go
+)
 
 select
-cd.date_datetime as gc_date,
-pay.Payments
-from
-cte_date cd
-left join
-    (
-      select
-      gc.created_at,
-      sum(gc.payments_amount::float) as Payments
-      from
-      cte_gc gc
-      group by
-      gc.created_at
-    ) pay
- on pay.created_at = cd.date_datetime
-where cd.date_datetime between '$StartDate' and '$EndDate'
-;
+    gc_date,
+    Payments,
+    Refunds
+from cte_goCardless_total_go
+order by rnk,gc_date ;
