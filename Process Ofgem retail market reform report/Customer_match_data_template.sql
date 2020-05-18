2d3b2f0315d55de2b71eab480d3ed70202a24e47
tolulopeadegunle <tolulope.adegunle@igloo.energy>
2020-05-18 23:27:19 +0100

    DMRE-1297

--- Process Ofgem retail market reform report/Customer_match_data_template.sql
+++ Process Ofgem retail market reform report/Customer_match_data_template.sql
@@ -17,7 +17,7 @@
 
 
       select 'Igloo Energy'                  as supplier_name,
-             nvl(crd.Report_Date, crd1.Report_Date)                       as date,
+              crd.Report_Date                       as date,
              '1-Gas'                         as gas_tariff_uid_1,
               null                           as gas_tariff_uid_2,
               null                           as gas_tariff_uid_3,
@@ -29,21 +29,21 @@
               null                           as electricity_tariff_uid_4,
               null                           as electricity_tariff_uid_5,
              case
-               when mpa.attributes_attributevalue = '_A' then 'east_england'
-               when mpa.attributes_attributevalue = '_B' then 'east_midlands'
-               when mpa.attributes_attributevalue = '_C' then 'london'
-               when mpa.attributes_attributevalue = '_D' then 'merseyside_and_north_wales'
-               when mpa.attributes_attributevalue = '_E' then 'midlands'
-               when mpa.attributes_attributevalue = '_F' then 'north_east'
-               when mpa.attributes_attributevalue = '_G' then 'north_west'
-               when mpa.attributes_attributevalue = '_H' then 'southern'
-               when mpa.attributes_attributevalue = '_J' then 'south_east'
-               when mpa.attributes_attributevalue = '_K' then 'south_wales'
-               when mpa.attributes_attributevalue = '_L' then 'south_west'
-               when mpa.attributes_attributevalue = '_M' then 'yorkshire'
-               when mpa.attributes_attributevalue = '_N' then 'south_scotland'
-               when mpa.attributes_attributevalue = '_P' then 'north_scotland'
-                 end                         as region,
+               when dcf.gsp = '_A' then 'east_england'
+               when dcf.gsp = '_B' then 'east_midlands'
+               when dcf.gsp = '_C' then 'london'
+               when dcf.gsp = '_D' then 'merseyside_and_north_wales'
+               when dcf.gsp = '_E' then 'midlands'
+               when dcf.gsp = '_F' then 'north_east'
+               when dcf.gsp = '_G' then 'north_west'
+               when dcf.gsp = '_H' then 'southern'
+               when dcf.gsp = '_J' then 'south_east'
+               when dcf.gsp = '_K' then 'south_wales'
+               when dcf.gsp = '_L' then 'south_west'
+               when dcf.gsp = '_M' then 'yorkshire'
+               when dcf.gsp = '_N' then 'south_scotland'
+               when dcf.gsp = '_P' then 'north_scotland'
+            end                         as region,
 
              'D'                              as payment_method_gas_1,
              null                              as payment_method_gas_2,
@@ -69,36 +69,17 @@
               null                                as  default_3_years_electricity_4,
               null                                as  default_3_years_electricity_5,
 
-             count(distinct(rth.account_id))      as number_accounts
-      from ref_meterpoints mp
-             inner join ref_meterpoints_attributes mpa on mp.account_id = mpa.account_id
-             inner join ref_tariff_history rth on mpa.account_id = rth.account_id
-             inner join ref_meterpoints mp1 on mp.account_id = mp1.account_id
-             --- inner join vw_acl_reg_gaselec_happy vreh on mp.account_id = vreh.account_id
+             count(distinct(dcf.account_id))      as number_accounts
+      from ref_calculated_daily_customer_file dcf
+             ---- inner join ref_meterpoints_attributes mpa on mp.account_id = mpa.account_id
+             ---- inner join ref_tariff_history rth on mpa.account_id = rth.account_id
+             ---- inner join ref_meterpoints mp1 on mp.account_id = mp1.account_id
+             ---- inner join vw_acl_reg_gaselec_happy vreh on mp.account_id = vreh.account_id
 
-             right join cte_report_dates crd on crd.date between substring(greatest(mp.supplystartdate, mp.associationstartdate), 1, 10)
-                            and substring(nvl(least(mp.supplyenddate, mp.associationenddate), sysdate), 1, 10)
-                            and mp.meterpointtype = 'E'
-
-            right join cte_report_dates crd1 on crd1.date between substring(greatest(mp1.supplystartdate, mp1.associationstartdate), 1, 10)
-                            and substring(nvl(least(mp1.supplyenddate, mp1.associationenddate), sysdate), 1, 10)
-                            and mp1.meterpointtype = 'G'
+             right join cte_report_dates crd on crd.date between substring( dcf.acc_ssd , 1, 10)
+                            and substring(nvl( dcf.acc_ed, sysdate), 1, 10)
 
 
-      WHERE mp.meterpointtype = 'E'
-        and mp1.meterpointtype = 'G'
-        and mpa.attributes_attributename = 'GSP'
-     /*
-        --- and (mp.supplyenddate is null or mp.supplyenddate > '2020-03-31')
-        and '2020-04-01' between substring(greatest(mp.supplystartdate, mp.associationstartdate), 1, 10)
-                            and substring(nvl(least(mp.supplyenddate, mp.associationenddate), sysdate), 1, 10)
-        and mp1.meterpointtype = 'G'
-
-        -- and (mp1.supplyenddate is null or mp1.supplyenddate > '2020-03-31')
-         and '2020-04-01' between substring(greatest(mp1.supplystartdate, mp1.associationstartdate), 1, 10)
-                            and substring(nvl(least(mp1.supplyenddate, mp1.associationenddate), sysdate), 1, 10)
-        and mpa.attributes_attributename = 'GSP'
-      */
-      group by nvl(crd.Report_Date, crd1.Report_Date), mpa.attributes_attributevalue
-      order by nvl(crd.Report_Date, crd1.Report_Date)::timestamp, mpa.attributes_attributevalue
+      group by  crd.Report_Date, dcf.gsp
+      order by crd.Report_Date::timestamp, dcf.gsp
       ;
