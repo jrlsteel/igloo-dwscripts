316bcc41c33d6c5ffe4227da0272b6288fbaea93
BenJesty <ben.jesty@igloo.energy>
2020-09-09 10:10:42 +0100

    DMRE-1368 - updated for prod schema (took off _uat suffixes)

--- MEETs/stratified_random_assignment_views.sql
+++ MEETs/stratified_random_assignment_views.sql
@@ -1,16 +1,4 @@
-create or replace view meets_signup_groups as
-select dcf_signup.*,
-       coalesce(existing_counts.num_control, 0)   as existing_control,
-       coalesce(existing_counts.num_treatment, 0) as existing_treatment,
-       case sign(coalesce((existing_counts.num_control - existing_counts.num_treatment), 0) +
-                 (dcf_signup.account_id % 2) - 0.5) # this is a tie-break for if num_control = num_treatment
 
-           when -1 then 'Control'
-           else 'Treatment' end                   as assigned_group
-from meets_account_strata dcf_signup
-         left join meets_group_counts as existing_counts on dcf_signup.gsp = existing_counts.gsp and
-                                                            dcf_signup.eac_band = existing_counts.eac_band
-order by account_id;
 
 
 create view meets_group_counts as
@@ -29,7 +17,7 @@ from meets_project_data mpd
                                            up.permission_level = 0
          inner join supply_contracts sc
                     on up.permissionable_id = sc.id and mpd.address_id = sc.supply_address_id
-         left join igloo_datawarehouse_uat.ref_calculated_daily_customer_file dcf
+         left join igloo_datawarehouse.ref_calculated_daily_customer_file dcf
                    on sc.external_id = dcf.account_id
 where mpd.opted_out is null
 group by gsp, eac_band;
@@ -43,7 +31,22 @@ select account_id,
            when eac_igloo_ca <= 3350 then 'S'
            when eac_igloo_ca <= 5161 then 'M'
            else 'L' end as eac_band
-from igloo_datawarehouse_uat.ref_calculated_daily_customer_file;
+from igloo_datawarehouse.ref_calculated_daily_customer_file;
+
+
+create view meets_signup_groups as
+select dcf_signup.*,
+       coalesce(existing_counts.num_control, 0)   as existing_control,
+       coalesce(existing_counts.num_treatment, 0) as existing_treatment,
+       case sign(coalesce((existing_counts.num_control - existing_counts.num_treatment), 0) +
+                 (dcf_signup.account_id % 2) - 0.5) # this is a tie-break for if num_control = num_treatment
+
+           when -1 then 'Control'
+           else 'Treatment' end                   as assigned_group
+from meets_account_strata dcf_signup
+         left join meets_group_counts as existing_counts on dcf_signup.gsp = existing_counts.gsp and
+                                                            dcf_signup.eac_band = existing_counts.eac_band
+order by account_id;
 
 # select *
 # from meets_signup_groups
