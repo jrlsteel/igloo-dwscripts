a35d9639b63b2062162a4fdbc744846dfd07e7da
BenJesty <ben.jesty@igloo.energy>
2020-09-22 14:46:02 +0100

    DCC-252 - Added clause in vw_metering_report_reads_info to limit estimates_elec_internal join to rows where islive=true

--- Metering/DMRE-1114/vw_metering_report_reads_info.sql
+++ Metering/DMRE-1114/vw_metering_report_reads_info.sql
@@ -1,8 +1,8 @@
 create or replace view vw_metering_report_reads_info as
 select max(
-         case when y.n = 1 then estimation_value else 0 end) over (partition by y.register_id) latest_eac,
+       case when y.n = 1 then ee.estimation_value else 0 end) over (partition by y.register_id) latest_eac,
        max(
-         case when y.n = 2 then estimation_value else 0 end) over (partition by y.register_id) previous_eac,
+       case when y.n = 2 then ee.estimation_value else 0 end) over (partition by y.register_id) previous_eac,
        y.account_id,
        y.meterpointnumber,
        y.registerreference,
@@ -19,10 +19,11 @@ from (select r.*,
              dense_rank() over (partition by account_id, register_id order by meterreadingdatetime desc) n,
              count(*) over (partition by account_id, register_id)                                        total_reads
       from ref_readings_internal_valid r) y
-       left outer join ref_estimates_elec_internal ee
-         on ee.account_id = y.account_id and y.meterpointnumber = ee.mpan and
-            y.registerreference = ee.register_id
-              and y.meterserialnumber = ee.serial_number and
-            ee.effective_from = y.meterreadingdatetime
-
-
+         left outer join ref_estimates_elec_internal ee
+                         on ee.account_id = y.account_id
+                             and y.meterpointnumber = ee.mpan
+                             and y.registerreference = ee.register_id
+                             and y.meterserialnumber = ee.serial_number
+                             and ee.effective_from = y.meterreadingdatetime
+                             and ee.islive
+where y.n <= 1;
