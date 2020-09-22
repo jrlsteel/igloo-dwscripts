add11b9cdc592c93b93a8232063128490e10bc54
BenJesty <ben.jesty@igloo.energy>
2020-09-22 17:23:33 +0100

    DCC-252 - updated vw_metering_report_psr to use the correct join and use a more efficient string replace function

--- Metering/DMRE-1114/vw_metering_report_psr.sql
+++ Metering/DMRE-1114/vw_metering_report_psr.sql
@@ -1,37 +1,16 @@
-drop view vw_metering_report_psr
-create or replace  view vw_metering_report_psr as select scoa.id,
+drop view vw_metering_report_psr;
+create or replace view vw_metering_report_psr as
+select scoa.id,
        scoa.external_id,
        up.user_id,
        attr.entity_type,
        attr.attribute_type_id,
        attr.attribute_value_id,
-       replace(replace(replace(replace(attr.attribute_custom_value,',',' '), '[', ''), ']', ''), '"', '') as psr
-from vw_supply_contracts_with_occ_accs scoa
-       inner join ref_cdb_user_permissions up
-         on scoa.id = up.permissionable_id and up.permissionable_type = 'App\\SupplyContract'
-       inner join ref_cdb_attributes attr on up.user_id = attr.entity_id and attr.attribute_type_id = 17
-
-
-       select scoa.id,
-       scoa.external_id,
-       up.user_id,
-       attr.entity_type,
-       attr.attribute_type_id,
-       attr.attribute_value_id,
-       json_array_length(attr.attribute_custom_value) as psr,
-       json_extract_path_text(attr.attribute_custom_value) as psrextract
+       regexp_replace(attr.attribute_custom_value, '[^0-9\,]') as psr -- remove characters other than digits and commas
 
 from vw_supply_contracts_with_occ_accs scoa
-       inner join ref_cdb_user_permissions up
-         on scoa.id = up.permissionable_id and up.permissionable_type = 'App\\SupplyContract'
-       inner join ref_cdb_attributes attr on up.user_id = attr.entity_id and attr.attribute_type_id = 17
+         inner join ref_cdb_user_permissions up
+                    on scoa.id = up.permissionable_id and up.permissionable_type = 'App\\SupplyContract'
+         inner join ref_cdb_attributes attr on up.user_id = attr.entity_id and attr.attribute_type_id = 17;
 
-
-select
-  TRIM(SPLIT_PART(attribute_custom_value, ',', 1)) AS tag
-
-select TRIM(SPLIT_PART(replace(replace(replace(attribute_custom_value, '[', ''), ']', ''), '"', ''), ',', 1))
-from ref_cdb_attributes
-where ref_cdb_attributes.attribute_type_id = 17
-
-
+alter table vw_metering_report_psr owner to igloo;
