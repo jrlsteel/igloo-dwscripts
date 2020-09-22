1c47402e62c057894e6fe9e7c31a414b6c4c6819
BenJesty <ben.jesty@igloo.energy>
2020-09-22 17:52:59 +0100

    DCC-252 - (vw_metering_report_psr) corrected join onto PSR data to use supply contract ID rather than user ID and adapted view to remove duplication.

--- Metering/DMRE-1114/vw_metering_report_psr.sql
+++ Metering/DMRE-1114/vw_metering_report_psr.sql
@@ -1,16 +1,22 @@
-drop view vw_metering_report_psr;
 create or replace view vw_metering_report_psr as
 select scoa.id,
        scoa.external_id,
        up.user_id,
-       attr.entity_type,
-       attr.attribute_type_id,
-       attr.attribute_value_id,
-       regexp_replace(attr.attribute_custom_value, '[^0-9\,]') as psr -- remove characters other than digits and commas
+       attr_psr.entity_type,
+       attr_psr.attribute_type_id,
+       attr_psr.attribute_value_id,
+       regexp_replace(attr_psr.attribute_custom_value, '[^0-9\,]') as psr -- remove characters other than digits and commas
 
 from vw_supply_contracts_with_occ_accs scoa
          inner join ref_cdb_user_permissions up
                     on scoa.id = up.permissionable_id and up.permissionable_type = 'App\\SupplyContract'
-         inner join ref_cdb_attributes attr on up.user_id = attr.entity_id and attr.attribute_type_id = 17;
+         inner join (
+    select *, row_number() over (partition by entity_id order by updated_at desc) as rn
+    from ref_cdb_attributes
+    where attribute_type_id = 17
+      and getdate() between nvl(effective_from, getdate() - 1) and
+        nvl(effective_to, getdate() + 1)
+) attr_psr on scoa.id = attr_psr.entity_id and attr_psr.rn = 1;
 
-alter table vw_metering_report_psr owner to igloo;
+alter table vw_metering_report_psr
+    owner to igloo;
