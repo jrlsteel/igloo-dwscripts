6599c45243069b4353ce1346ba10b23ff2ca8ffa
BenJesty <ben.jesty@igloo.energy>
2020-11-10 17:06:19 +0000

    DMRE-1546 - added commands to set initial values of last_updated_igloo to the creation date of the entity

--- GoCardless/DMRE-1546_table_updates.sql
+++ GoCardless/DMRE-1546_table_updates.sql
@@ -6,6 +6,8 @@ alter table ref_fin_gocardless_mandates
     add column last_updated_igloo timestamp;
 alter table ref_fin_gocardless_mandates
     add column etlchange timestamp;
+-- the "where true" below is to suppress a warning of updating the whole table, which is the intended behaviour here
+update ref_fin_gocardless_mandates set last_updated_igloo = created_at where true;
 
 --- Mandates Audit ---
 alter table ref_fin_gocardless_mandates_audit
@@ -14,6 +16,8 @@ alter table ref_fin_gocardless_mandates_audit
     add column last_updated_igloo timestamp;
 alter table ref_fin_gocardless_mandates_audit
     add column etlchange timestamp;
+-- the "where true" below is to suppress a warning of updating the whole table, which is the intended behaviour here
+update ref_fin_gocardless_mandates_audit set last_updated_igloo = created_at where true;
 
 --- Payments ---
 alter table ref_fin_gocardless_payments
@@ -22,6 +26,8 @@ alter table ref_fin_gocardless_payments
     add column last_updated_igloo timestamp;
 alter table ref_fin_gocardless_payments
     add column etlchange timestamp;
+-- the "where true" below is to suppress a warning of updating the whole table, which is the intended behaviour here
+update ref_fin_gocardless_payments set last_updated_igloo = created_at where true;
 
 --- Refunds ---
 alter table ref_fin_gocardless_refunds
@@ -30,6 +36,8 @@ alter table ref_fin_gocardless_refunds
     add column last_updated_igloo timestamp;
 alter table ref_fin_gocardless_refunds
     add column etlchange timestamp;
+-- the "where true" below is to suppress a warning of updating the whole table, which is the intended behaviour here
+update ref_fin_gocardless_refunds set last_updated_igloo = created_at where true;
 
 --- Refunds Audit ---
 alter table ref_fin_gocardless_refunds_audit
@@ -38,6 +46,8 @@ alter table ref_fin_gocardless_refunds_audit
     add column last_updated_igloo timestamp;
 alter table ref_fin_gocardless_refunds_audit
     add column etlchange timestamp;
+-- the "where true" below is to suppress a warning of updating the whole table, which is the intended behaviour here
+update ref_fin_gocardless_refunds_audit set last_updated_igloo = created_at where true;
 
 --- Subscriptions ---
 alter table ref_fin_gocardless_subscriptions
@@ -46,6 +56,8 @@ alter table ref_fin_gocardless_subscriptions
     add column last_updated_igloo timestamp;
 alter table ref_fin_gocardless_subscriptions
     add column etlchange timestamp;
+-- the "where true" below is to suppress a warning of updating the whole table, which is the intended behaviour here
+update ref_fin_gocardless_subscriptions set last_updated_igloo = created_at where true;
 
 --- Subscriptions Audit ---
 alter table ref_fin_gocardless_subscriptions_audit
@@ -54,3 +66,5 @@ alter table ref_fin_gocardless_subscriptions_audit
     add column last_updated_igloo timestamp;
 alter table ref_fin_gocardless_subscriptions_audit
     add column etlchange timestamp;
+-- the "where true" below is to suppress a warning of updating the whole table, which is the intended behaviour here
+update ref_fin_gocardless_subscriptions_audit set last_updated_igloo = created_at where true;
