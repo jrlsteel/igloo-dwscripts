954aedb8e86857bff91482b6f1a7594dd0ad3e08
BenJesty <ben.jesty@igloo.energy>
2020-11-10 16:53:55 +0000

    DMRE-1546 - added table update SQL

---
+++ GoCardless/DMRE-1546_table_updates.sql
@@ -0,0 +1,56 @@
+--- UPDATE Go Cardless Tables ---
+--- Mandates ---
+alter table ref_fin_gocardless_mandates
+    drop column etlchange;
+alter table ref_fin_gocardless_mandates
+    add column last_updated_igloo timestamp;
+alter table ref_fin_gocardless_mandates
+    add column etlchange timestamp;
+
+--- Mandates Audit ---
+alter table ref_fin_gocardless_mandates_audit
+    drop column etlchange;
+alter table ref_fin_gocardless_mandates_audit
+    add column last_updated_igloo timestamp;
+alter table ref_fin_gocardless_mandates_audit
+    add column etlchange timestamp;
+
+--- Payments ---
+alter table ref_fin_gocardless_payments
+    drop column etlchange;
+alter table ref_fin_gocardless_payments
+    add column last_updated_igloo timestamp;
+alter table ref_fin_gocardless_payments
+    add column etlchange timestamp;
+
+--- Refunds ---
+alter table ref_fin_gocardless_refunds
+    drop column etlchange;
+alter table ref_fin_gocardless_refunds
+    add column last_updated_igloo timestamp;
+alter table ref_fin_gocardless_refunds
+    add column etlchange timestamp;
+
+--- Refunds Audit ---
+alter table ref_fin_gocardless_refunds_audit
+    drop column etlchange;
+alter table ref_fin_gocardless_refunds_audit
+    add column last_updated_igloo timestamp;
+alter table ref_fin_gocardless_refunds_audit
+    add column etlchange timestamp;
+
+--- Subscriptions ---
+alter table ref_fin_gocardless_subscriptions
+    drop column etlchange;
+alter table ref_fin_gocardless_subscriptions
+    add column last_updated_igloo timestamp;
+alter table ref_fin_gocardless_subscriptions
+    add column etlchange timestamp;
+
+--- Subscriptions Audit ---
+alter table ref_fin_gocardless_subscriptions_audit
+    drop column etlchange;
+alter table ref_fin_gocardless_subscriptions_audit
+    add column last_updated_igloo timestamp;
+alter table ref_fin_gocardless_subscriptions_audit
+    add column etlchange timestamp;
