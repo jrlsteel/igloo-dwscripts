3a25a7c057d1dc9578a1b0e5c419619724d0799d
Jonathan Steel <jonathan.steel@igloo.energy>
2020-11-16 12:06:21 +0000

    DMRE-527

--- INF-527- Debt-Age/ref_account_debt_status.sql
+++ INF-527- Debt-Age/ref_account_debt_status.sql
@@ -1,19 +1,23 @@
 create table ref_account_debt_status
 (
-	user_id integer encode az64,
-	contract_id integer encode az64,
-	contract_type varchar(256),
-	current_account_balance integer encode az64,
-	current_direct_debit integer encode az64,
-	bills_outstanding integer encode az64,
-	transaction_id integer encode az64,
+	user_id bigint encode az64 distkey,
+	contract_id bigint encode az64,
+	contract_type varchar(13),
+	current_account_balance double precision,
+	current_direct_debit bigint encode az64,
+	bills_outstanding bigint encode az64,
+	transaction_id bigint encode az64,
 	bill_date timestamp encode az64,
-	bill_age integer encode az64
+	bill_age bigint encode az64,
+	hold_days integer encode az64,
+	adjusted_bill_age bigint encode az64,
+	bill_amount integer encode az64,
+	value_paid_off bigint encode az64,
+	outstanding_value bigint encode az64,
+	payment_method varchar(4)
 )
+diststyle key
 ;
-
-
-
 
 alter table ref_account_debt_status owner to igloo
 ;
