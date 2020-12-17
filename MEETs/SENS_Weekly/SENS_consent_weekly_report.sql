d2a852d73f0a84fbc210354854f52884229f3337
BenJesty <ben.jesty@igloo.energy>
2020-12-17 13:56:30 +0000

    Updated meets reports into views

--- MEETs/SENS_Weekly/SENS_consent_weekly_report.sql
+++ MEETs/SENS_Weekly/SENS_consent_weekly_report.sql
@@ -1,26 +1,29 @@
 create or replace view vw_sens_consent_weekly_report as
-select mpd.id                                                           as ext_participant_id,
-       addr.addressline1                                                as Address_line1,
-       addr.addressline2                                                as Address_line2,
-       addr.addressline3                                                as Address_line3,
-       addr.addressline4                                                as Address_line4,
+select mpd.id                                                                                as ext_participant_id,
+       addr.addressline1                                                                     as Address_line1,
+       addr.addressline2                                                                     as Address_line2,
+       addr.addressline3                                                                     as Address_line3,
+       addr.addressline4                                                                     as Address_line4,
        addr.postaltown,
        addr.county,
        addr.postcode,
-       u.first_name                                                     as firstname,
-       u.last_name                                                      as surname,
-       listagg(mp.meterpointnumber, ', ')                               as mpan,
+       u.first_name                                                                          as firstname,
+       u.last_name                                                                           as surname,
+       listagg(mp.meterpointnumber, ', ')                                                    as mpan,
        addr.uprn,
-       (nvl(mpd.opted_out, dcf.acc_ed, getdate() + 1) > getdate())::int as consent_provided,
-       mpd.opted_in                                                     as consent_start_date,
-       nvl(mpd.opted_out, dcf.acc_ed)                                   as consent_end_date,
-       'Igloo'                                                          as consent_source,
-       1                                                                as auth_completed,
-       mpd.opted_in                                                     as auth_date,
-       mpd.move_in_date                                                 as move_in_date,
-       'SENS'                                                           as project_id,
-       'sens-igloo'                                                     as trial_id,
-       mpd."group"                                                      as trial_group
+       (nvl(mpd.opted_out, dcf.acc_ed, getdate() + 1) > getdate())::int                      as consent_provided,
+       mpd.opted_in                                                                          as consent_start_date,
+       nvl(mpd.opted_out, dcf.acc_ed)                                                        as consent_end_date,
+       'Igloo'                                                                               as consent_source,
+       1                                                                                     as auth_completed,
+       mpd.opted_in                                                                          as auth_date,
+       mpd.move_in_date                                                                      as move_in_date,
+       'SENS'                                                                                as project_id,
+       'sens-igloo'                                                                          as trial_id,
+       mpd."group"                                                                           as trial_group,
+       case when count(dev_ids.device_id) = count(mp.meterpointnumber) then 'Y' else 'N' end as dcc_enrolled_meter,
+       u.phone_number,
+       u.email
 from aws_s3_stage2_extracts.stage2_cdbmeetsprojectdata mpd
          left join (select id,
                            replace(trim(', ' from nvl(sub_building_name_number, '') ||
@@ -45,8 +48,17 @@ from aws_s3_stage2_extracts.stage2_cdbmeetsprojectdata mpd
                                                 nvl(mpd.opted_out, dcf.acc_ed, trunc(getdate())) between
                                                     greatest(supplystartdate, associationstartdate) and
                                                     nvl(least(supplyenddate, associationenddate), getdate() + 1)
+         left join public.vw_smart_device_id_mpxn_map dev_ids on dev_ids.mpxn = mp.meterpointnumber
 group by mpd.id, addr.addressline1, addr.addressline2, addr.addressline3, addr.addressline4, addr.postaltown,
          addr.county, addr.postcode, u.first_name, u.last_name, addr.uprn, consent_provided, mpd.opted_in,
-         consent_end_date, mpd.move_in_date, mpd."group"
-order by ext_participant_id
-    with no schema binding;
+         consent_end_date, mpd.move_in_date, mpd."group", u.email, u.phone_number
+order by mpd.id
+with no schema binding;
+
+select *
+from vw_sens_consent_weekly_report
+;
+where ext_participant_id in (select ext_participant_id from vw_sens_consent_weekly_report where not dcc_enrolled_meter)
+
+grant usage on schema aws_smart_stage2_extracts to igloo_dw_uat_user
+
--- MEETs/SENS_Weekly/SENS_withdrawal_of_consent.sql
+++ MEETs/SENS_Weekly/SENS_withdrawal_of_consent.sql
@@ -51,5 +51,48 @@ group by mpd.id,
          u.first_name,
          u.last_name,
          addr.uprn
-order by ext_participant_id
-    with no schema binding
+order by mpd.id
+with no schema binding
+
+
+-- from Grafana
+create or replace view vw_sens_withdrawal_of_consent as
+select mpd.id                              as ext_participant_id,
+       null                                as SERL_participant_ID,
+       coalesce(mpd.opted_out, dcf.acc_ed) as Consent_Withdrawal_Date,
+       --addr.sub_building_name_number,
+       addr.building_name_number           as Address_line1,
+       --addr.dependent_thoroughfare,
+       addr.thoroughfare                   as Address_line2,
+       addr.double_dependent_locality      as Address_line3,
+       addr.dependent_locality             as Address_line4,
+       addr.post_town                      as postaltown,
+       addr.county,
+       addr.postcode,
+       u.first_name                        as firstname,
+       u.last_name                         as surname,
+       'SENS'                              as source,
+       'FullErasure'                       as gdprtype,
+       listagg(mp.meterpointnumber, ', ')  as mpan,
+       addr.uprn
+from aws_s3_stage2_extracts.stage2_cdbmeetsprojectdata mpd
+         left join public.ref_cdb_addresses addr on mpd.address_id = addr.id
+         left join public.ref_cdb_users u on mpd.user_id = u.id
+         left join public.ref_cdb_user_permissions up_sc on up_sc.user_id = u.id and up_sc.permission_level = 0 and
+                                                     up_sc.permissionable_type ilike 'app%supplycontract'
+         left join public.ref_cdb_supply_contracts sc on up_sc.permissionable_id = sc.id
+         left join public.ref_meterpoints mp on mp.account_id = sc.external_id and mp.meterpointtype = 'E' and
+                                         nvl(mpd.opted_out, trunc(getdate())) between
+                                             greatest(supplystartdate, associationstartdate) and
+                                             nvl(least(supplyenddate, associationenddate), getdate() + 1)
+         left join public.ref_calculated_daily_customer_file dcf on sc.external_id = dcf.account_id
+where Consent_Withdrawal_Date is not null
+  AND dcf.acc_ed is null
+group by mpd.id, Consent_Withdrawal_Date,
+         --addr.sub_building_name_number,
+         addr.building_name_number,
+         --addr.dependent_thoroughfare,
+         addr.thoroughfare, addr.double_dependent_locality, addr.dependent_locality, addr.post_town, addr.county,
+         addr.postcode, u.first_name, u.last_name, addr.uprn
+order by mpd.id
+with no schema binding
