96bd39a931f4478b4489f19ee2743b3786d968e6
Simon Hunt <simon.hunt@igloo.energy>
2021-02-10 15:00:12 +0000

    Merged in feature/ENGY-28 (pull request #6)
    ENGY-28: Add payment day to debt management report
    
    * feat: add payment day (if on direct debit) to the debt management report
    
    * refactor: rename cte_variables to cte_constants and make constants upper case
    
    Approved-by: Ben Jesty

--- reporting/Debt/aged_debt_report.sql
+++ reporting/Debt/aged_debt_report.sql
@@ -1,43 +1,55 @@
-with cte_variables as (
+with cte_constants as (
         select
-        14::INT                                                                         as day_zero_offset
+        14::INT                                                                         as DAY_ZERO_OFFSET
+    ),
+    cte_payment_day as (
+        select all_ids.igl_acc_id,
+               sub.day_of_month,
+               row_number() over (partition by igl_acc_id) as rownum
+        from vw_gocardless_customer_id_mapping all_ids
+              left join public.ref_fin_gocardless_mandates man
+                    on all_ids.client_id = man.customerid and
+                       man.status in ('active', 'submitted')
+              left join public.ref_fin_gocardless_subscriptions sub
+                    on sub.mandate = man.mandate_id and
+                       sub.status = 'active'
     ),
     cte_debt_ages as (
         select vbs.contract_id                                                          as contract_id,
-               nvl(sum(case when bill_age <= day_zero_offset
+               nvl(sum(case when bill_age <= DAY_ZERO_OFFSET
                         then outstanding_value
                    end), 0)/100::float                                                  as not_overdue,
-               nvl(sum(case when bill_age between (1 + day_zero_offset)
-                                      and (29 + day_zero_offset)
+               nvl(sum(case when bill_age between (1 + DAY_ZERO_OFFSET)
+                                      and (29 + DAY_ZERO_OFFSET)
                         then outstanding_value
                    end), 0)/100::float                                                  as from0to29days,
-               nvl(sum(case when bill_age between (30 + day_zero_offset)
-                                      and (89 + day_zero_offset)
+               nvl(sum(case when bill_age between (30 + DAY_ZERO_OFFSET)
+                                      and (89 + DAY_ZERO_OFFSET)
                         then outstanding_value
                    end), 0)/100::float                                                  as from30to89days,
-               nvl(sum(case when bill_age between (90 + day_zero_offset)
-                                      and (179 + day_zero_offset)
+               nvl(sum(case when bill_age between (90 + DAY_ZERO_OFFSET)
+                                      and (179 + DAY_ZERO_OFFSET)
                         then outstanding_value
                    end), 0)/100::float                                                  as from90to179days,
-               nvl(sum(case when bill_age between (180 + day_zero_offset)
-                                      and (364 + day_zero_offset)
+               nvl(sum(case when bill_age between (180 + DAY_ZERO_OFFSET)
+                                      and (364 + DAY_ZERO_OFFSET)
                         then outstanding_value
                    end), 0) /100::float                                                 as from180to364days,
-               nvl(sum(case when bill_age > 365 + day_zero_offset
+               nvl(sum(case when bill_age > 365 + DAY_ZERO_OFFSET
                         then outstanding_value
                    end), 0)/100::float                                                  as over365days,
                nvl(max(case when outstanding_value > 0
-                        and bill_age > day_zero_offset
-                        then bill_age - day_zero_offset
+                        and bill_age > DAY_ZERO_OFFSET
+                        then bill_age - DAY_ZERO_OFFSET
                         else 0
                    end), 0)::int                                                        as debt_age,
                nvl(max(case when outstanding_value > 0
-                        and adjusted_bill_age > day_zero_offset
-                        then adjusted_bill_age - day_zero_offset
+                        and adjusted_bill_age > DAY_ZERO_OFFSET
+                        then adjusted_bill_age - DAY_ZERO_OFFSET
                         else 0
                    end), 0)::int                                                        as adjusted_debt_age
         from vw_debit_status as vbs
-                cross join cte_variables variables
+                cross join cte_constants constants
         group by contract_id
     )
 select dcf.account_id                                                           as account_id,
@@ -50,6 +62,7 @@ select dcf.account_id                                                           
        dcf.first_payment_success                                                as first_payment_success,
        dcf.payment_in_last_month                                                as payment_in_last_month,
        debt_status.payment_method                                               as payment_method,
+       payment_day.day_of_month                                                 as payment_day,
        dcf.occupier_account                                                     as occupier_account,
        dcf.balance                                                              as balance,
        case
@@ -69,7 +82,10 @@ from cte_debt_ages debt_ages
         left join ref_calculated_daily_customer_file as dcf
             on debt_ages.contract_id = dcf.account_id
         left join ref_account_debt_status debt_status
-                on dcf.account_id = debt_status.contract_id
+            on dcf.account_id = debt_status.contract_id
+        left join cte_payment_day payment_day
+            on dcf.account_id = payment_day.igl_acc_id and
+               payment_day.rownum = 1 -- for a small number of accounts, more than one row is returned
 
 
 
